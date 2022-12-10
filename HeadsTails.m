2c883d8a747b97450ceec1285c641b3e6adfb0eb
AlperErensir <120280220+AlperErensir@users.noreply.github.com>
2022-12-11 00:14:33 +0300

    Add files via upload

--- HeadsTails.m
+++ HeadsTails.m
@@ -2,8 +2,9 @@ clear all, clc
 money = 100000;
 count = 0;
 total_loss = 0;
-number_of_games = 100;   % ## Adjustable parameter ##
-gamble_amount = 1000;    % ## Adjustable parameter ##
+% ## Adjustable parameters ##
+number_of_games = 100;   
+gamble_amount = 1000;    
 t = 1:number_of_games;
 xx = 0; j = 1;  k = 0; n=0;
 negative_value_count = 0;
@@ -14,22 +15,26 @@ while k<number_of_games
     for i = 1:number_of_games
 
         variable = rand;
-
-        if variable < 0.50                                 %Winning case
-            money = money + gamble_amount + total_loss;     %
-            count = 0;                                      %
-            total_loss = 0;                                 %
-        else                                                %Losing case
-            money = money - gamble_amount - total_loss;     %
-            count = count + 1;                              %
-            total_loss = total_loss + count*gamble_amount;  %
+        
+        % Winning case.
+        if variable < 0.50                                 
+            money = money + gamble_amount + total_loss;     
+            count = 0;                                      
+            total_loss = 0;
+        % Losing case.
+        else                                                
+            money = money - gamble_amount - total_loss;     
+            count = count + 1;                              
+            total_loss = total_loss + count*gamble_amount;  
         end
 
-        if money == 0 && money < 0                          %Case of bankrupt
+        % Case of bankrupt.
+        if money == 0 && money < 0                          
             i = number_of_games;
         end
 
-        if k<number_of_games                                %Playing the game with different amount of times
+        % Playing the game with different amount of times.
+        if k<number_of_games                                
             if i==number_of_games
                 final_money_matrix(1,j) = money;
                 j = j + 1;
